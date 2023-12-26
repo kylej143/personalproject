e425d03ce66283c91e2665f2be737ec8716a2ddc
kylej143 <kylejoung23@gmail.com>
2023-12-25 22:08:23 -0500

    basic set up

---
+++ src/ImageConverter.py
@@ -0,0 +1,43 @@
+import numpy
+from PIL import Image
+from numpy import asarray
+
+
+def imageToArray(path) -> numpy.ndarray:
+    """
+    Converts an image to len(input_array) = num of rows, len(input_array[i]) = num of columns
+
+    (In other words the image is converted up to down, then in the rows left to right)
+
+    [R, G, B, A] format in each instance of input_array[i][i].
+
+    Parameter
+        path (str): The path of the image
+
+    Returns
+        numpy.ndarray(): the image in an array format
+    """
+    image = Image.open(path)
+    return asarray(image)
+
+
+
+def arrayToImage(array) -> None:
+    """
+    Converts a ndarray object into a png file, saves it as a test_result.png
+
+    Parameter
+        array (ndarray): The numpy.ndarray that is being converted into an image
+    """
+    output_image = Image.fromarray(array)
+    output_image.save("test_result.png")
+
+
+def arrayInfo(array) -> str:
+    """
+    Returns the dimension of the array (image)
+
+    Parameter
+        array (ndarray): The numpy.ndarray that is being converted into an image
+    """
+    return "width: " + str(array.shape[1]) + "\nheight: " + str(array.shape[0])
---
+++ src/main.py
@@ -0,0 +1,9 @@
+import ImageConverter
+
+image1 = "image/shape1.png"
+image2 = "image/shape2.png"
+
+input_array = ImageConverter.imageToArray(image1)
+print(ImageConverter.arrayInfo(input_array))
+
+
